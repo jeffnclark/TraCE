74a6aa8593b59cca5bb7fe81f455be136b46746b
jeffnclark <jeff.clark@bristol.ac.uk>
2023-12-14 18:15:18 +0000

    Refactor

--- helpers/funcs.py
+++ helpers/funcs.py
@@ -136,9 +136,6 @@ def score1(x0, x1, x_prime, x_star=None, method='dot'):
             norm_v = v / np.linalg.norm(v)
             length = np.sqrt(2 - 2 * theta)
             S = np.dot((norm_v_prime - norm_v_star) / length, norm_v)
-            # S1 = (np.dot(v, v_prime) / (np.linalg.norm(v) * np.linalg.norm(v_prime))) * (np.linalg.norm(v) / np.linalg.norm(v_prime))
-            # S2 = (np.dot(v, v_star) / (np.linalg.norm(v) * np.linalg.norm(v_star))) * (np.linalg.norm(v) / np.linalg.norm(v_star))
-            # S = (S1 - S2) / 2
 
     if method == 'avg':
         S = 0
@@ -188,8 +185,8 @@ def score1(x0, x1, x_prime, x_star=None, method='dot'):
 
 def news(x):
     """
-    Calculates the NEWS score, whislt capturing the good counterfactual and bad counterfactual in parallel
-    :param x: vector of NEWS values (resp. rate, ox. sat., ox. sup., temp., blood pres., hrt rt, consciousness)
+    Calculates the NEWS score, whilst capturing the good counterfactual and bad counterfactual in parallel
+    :param x: vector of NEWS values for all features
     :return: news score, good counterfactual x_prime, bad counterfactual x_star
     """
     # set up values for calculation
@@ -314,3 +311,203 @@ def cumulative_average_trace(scores):
     '''
     df = pd.DataFrame(scores, columns=['score'])
     return df['score'].expanding().mean().to_numpy()
+
+
+def normalize_data(df_data):
+    '''
+    function used to normalize the dataframe
+    input: df_data -  pd dataframe to be normalized
+    returns : normalized_df_data - dataframe after normalization
+    '''
+    # Calculate separate mean and standard deviation values
+    df_mean = df_data.mean()
+    df_std = df_data.std()
+    normalized_df_data = (df_data-df_mean)/df_std
+    return normalized_df_data, df_mean, df_std
+
+
+def remove_empty_rows(filepath):
+    '''
+    Function to get the data where there is the data with only the empty rows
+    CURRENTLY UNUSED
+    '''
+    df_data = pd.read_csv(filepath,
+                          header=0)
+
+    desired_variables = ['stay_id',
+                         'biocarbonate', 'bloodOxygen', 'bloodPressure', 'bun', 'creatinine',
+                         'fio2', 'haemoglobin', 'heartRate', 'motorGCS', 'eyeGCS',
+                         'potassium', 'respiratoryRate', 'sodium', 'Temperature [C]',
+                         'verbalGCS', 'age', 'gender',
+                         'hours_since_admission',
+                         'RFD']
+
+    # Obtain only the variables of interes
+    desired_df_data = df_data[desired_variables]
+    # Make sure to use desired_df_columnss for the len threshold
+    filtered_df_data = desired_df_data.dropna(
+        thresh=len(desired_df_data.columns))
+    concatenated = pd.concat([desired_df_data, filtered_df_data])
+    different_rows = concatenated.drop_duplicates(keep=False)
+    # Reset index of the resulting dataframe
+    different_rows.reset_index(drop=True, inplace=True)
+    return filtered_df_data, different_rows
+
+
+def balancing_classes(
+        data,
+        data_labels):
+    '''
+    Used to balance the data and the data labels
+    input: data - can be the data or the data labels which we want to obtain values of
+    data_labels- used to obtain the separate classes of interest
+    return: balanced_df_data - data which is balanced in terms of the different classes
+            balanced_df_labels - labels which is balanced in terms of the different classes
+    '''
+    # Separate the data into labels, an example here is the ready for discharge ICU case study
+    neutral_data = data[data_labels['RFD'] == 0]
+    positive_data = data[data_labels['RFD'] == 1]
+    negative_data = data[data_labels['RFD'] == 2]
+
+    # Get the minimum values
+    values = [len(neutral_data), len(negative_data), len(positive_data)]
+    min_value = min(values)
+    # Shorten the array based on the labels
+    neutral_data = neutral_data.head(min_value)
+    negative_data = negative_data.head(min_value)
+    positive_data = positive_data.head(min_value)
+
+    # Shorten the labels
+    neutral_labels = data_labels[data_labels['RFD'] == 0].head(min_value)
+    positive_labels = data_labels[data_labels['RFD'] == 1].head(min_value)
+    negative_labels = data_labels[data_labels['RFD'] == 2].head(min_value)
+
+    # Concatenate the data
+    balanced_df_data = pd.concat(
+        (neutral_data, negative_data, positive_data), axis=0)
+    balanced_df_labels = pd.concat(
+        (neutral_labels, negative_labels, positive_labels), axis=0)
+
+    return balanced_df_data, balanced_df_labels
+
+
+def nan_post_processing_data(df_data, columns):
+    '''
+    Function for post processing the dataframe with all the proceessed values (eg removing nans)
+    input: 
+    df_data -  dataframe which contain the values of all the processed files
+    columns -  column of interest to perform data processing on
+    returns:
+    df_data -  post-processed dataframe
+    '''
+    postprocessSettings = 2
+
+    # for column in df_data:
+    for column in columns:
+        # determine replacement values
+        if postprocessSettings == 0:
+            # leave empty field empty
+            newValue = ''
+        elif postprocessSettings == 1:
+            # replace empty field by a zero
+            newValue = 0
+        elif postprocessSettings == 2:
+            # replace empty field with
+            # the mean value for column containing numerical values,
+            # and the most frequent value in columns
+            # with non-numerical/categorical values
+            if 'GCS' in column:
+                newValue = df_data[column].mode()
+                # FOR THE CASE OF THE MODE, NEED TO GET THE FIRST INDEX
+                df_data[column] = df_data[column].fillna(newValue[0])
+            elif 'gender' in column:
+                newValue = df_data[column].mode()
+                df_data[column] = df_data[column].fillna(newValue[0])
+            else:
+                newValue = df_data[column].mean()
+                df_data[column] = df_data[column].fillna(newValue)
+    return df_data
+
+
+def change_categorical(df, categorical_features):
+    '''
+    function: Used to change data variables to categorical type (for the case of passing to DiCE)
+    '''
+    for column in categorical_features:
+        df[column] = df[column].astype('category')
+    return df
+
+
+def cf_generator(df, classifier):
+    ''' Function to make the object for the generation of the counterfactuals
+    input: df: dataframe containing all the data, also includes the labels
+    classifier: trained deep learnng classifier
+    retirms: exp_genetic_mimic - object to generate DiCE counterfactuals
+            features_to_vary - features which I want to vary when generating counterfactuals
+    '''
+
+    continuous_features_mimic = df.drop(
+        columns=['RFD', 'eyeGCS', 'motorGCS', 'verbalGCS', 'gender'], axis=1).columns.tolist()
+
+    # Feature to vary during counterfactual generation
+    features_to_vary = df.drop(columns=['RFD', 'age'], axis=1).columns.tolist()
+    d_mimic = dice_ml.Data(dataframe=df,
+                           continuous_features=continuous_features_mimic,
+                           outcome_name='RFD')
+
+    # We provide the type of model as a parameter (model_type)
+    m_mimic = dice_ml.Model(
+        model=classifier, backend="sklearn", model_type='classifier')
+    exp_genetic_mimic = Dice(d_mimic, m_mimic)  # , method="genetic")
+    return exp_genetic_mimic, features_to_vary
+
+
+def initial_icu_processing(filepath, features):
+    '''
+    Function used to perform initial preprocessing of the data, this includes:
+    choosing the correct columns of interest,
+    changing the label to be negative outocome label from -1 to 2 (for the purpose of DiCE),
+    changing the gender label,
+    as well as filling in missing values in the data
+    input:
+        filepath - path of the csv file which cotains the data
+        features - which features you want to extract and use
+
+    return:desired_df_data -  dataframe after processing the data 
+    '''
+    df_data = pd.read_csv(filepath,
+                          header=0)
+    # Get the variables of interest
+
+    # Obtain only the variables of interes
+    desired_df_data = df_data[features]
+    # Replace values of -1 with 2 (NEGATIVE LABELLED CLASSES DOES NOT SEEM TO WORK WITH DICE)
+    desired_df_data['RFD'].replace(-1, 2, inplace=True)
+
+    # Change gender category from string to float
+    desired_df_data['gender'].replace('M', 0, inplace=True)
+    desired_df_data['gender'].replace('F', 1, inplace=True)
+    desired_df_data['gender'] = desired_df_data['gender'].astype('float')
+
+    # Update the mean and the standard deviation for the data
+    columns_to_process = desired_df_data.drop(
+        columns=['RFD', 'stay_id', 'hours_since_admission'], axis=1).columns.tolist()
+    # columns_to_process=desired_df_data.columns.tolist()
+
+    neutral_data = desired_df_data[desired_df_data['RFD'] == 0]
+    positive_data = desired_df_data[desired_df_data['RFD'] == 1]
+    negative_data = desired_df_data[desired_df_data['RFD'] == 2]
+
+    # Fill in missing values of the data
+    processed_neutral_data = nan_post_processing_data(
+        neutral_data, columns_to_process)
+    processed_negative_data = nan_post_processing_data(
+        negative_data, columns_to_process)
+    processed_positive_data = nan_post_processing_data(
+        positive_data, columns_to_process)
+
+    desired_df_data.update(processed_neutral_data)
+    desired_df_data.update(processed_negative_data)
+    desired_df_data.update(processed_positive_data)
+
+    return desired_df_data
